147c64c7d99eb09f65f719aac1e9fc36eb97f61b
SinySs <dimakrytoi8@gmail.com>
2022-03-19 02:29:01 +0300

    add stucts and enums. add struct init

--- lx.c
+++ lx.c
@@ -13,7 +13,42 @@ struct buffer {
     int max_len;
     int num_of_token;
 
-} ;
+};
+
+enum lexem_kind_t{
+    OP,
+    BRACE,
+    NUM
+};
+
+enum operation_t{
+    ADD,
+    SUB,
+    MUL,
+    DIV
+};
+
+enum braces_t{
+    LBRAC,
+    RBRAC
+};
+
+
+struct lexem_t {
+    enum lexem_kind_t kind;
+    union {
+        enum operation_t op;
+        enum braces_t b;
+        int num;
+    } lex;
+};
+
+struct lex_array_t {
+
+    struct lexem_t *lexems;
+    int size, capacity;
+
+};
 
 void change_size(struct buffer *buf, int new_size)
 {
@@ -38,8 +73,8 @@ int read_in_buf(struct buffer *buf)
 
     a = getchar();
 
-    //while(a != EOF || a != '\n') {
-    while(a != '\n') {
+    while(a != EOF && a != '\n') {
+    //while(a != '\n') {
 
         buf->text[buf->len] = a;
         buf->len++;
@@ -56,17 +91,34 @@ int read_in_buf(struct buffer *buf)
         }
     }
 
+    buf->text[buf->len] = '\0';
+    change_size(buf, buf->len);
 
+}
+
+struct lex_array_t lex_init(int num_of_lex)
+{
+   struct lex_array_t new_arr;
+
+   new_arr.lexems = calloc(num_of_lex, sizeof(struct lexem_t));
+   new_arr.capacity = num_of_lex;
+   new_arr.size = 0;
+
+   return new_arr;
 }
 
 int main()
 {
     struct buffer Buf;
+    struct lex_array_t Lexems;
+
 
     read_in_buf(&Buf);
     for(int i = 0; i < Buf.len; i++) {
         printf("%c", Buf.text[i]);
     }
+
+    Lexems = lex_init(Buf.num_of_token);
 
     free(Buf.text);
 
